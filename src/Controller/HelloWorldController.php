2eec85b7582dfb97f170a3665f3237099c7f795b
David Suys <david@icontroller.eu>
2018-01-26 10:01:48 +0100

    inject twig in controller constructor instead of action

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -8,18 +8,23 @@ use Twig\Environment;
 
 class HelloWorldController
 {
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     /**
      * @Route("/hello", name="hello", methods={"GET"})
-     *
-     * @param Environment $twig
      *
      * @return Response
      * @throws \Twig_Error_Loader
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    public function hello(Environment $twig): Response
+    public function hello(): Response
     {
-        return new Response($twig->render('hello.html.twig', ['name' => 'David']));
+        return new Response($this->twig->render('hello.html.twig', ['name' => 'David']));
     }
 }
