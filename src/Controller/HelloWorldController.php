1a005d40ff5a2c2df6fc32c0437cb787ff870e9e
David Suys <david@icontroller.eu>
2018-01-26 09:43:55 +0100

    use annotations for controller + add maker bundle

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class HelloWorldController
 {
+    /**
+     * @Route("/hello", name="hello", methods={"GET"})
+     *
+     * @return Response
+     */
     public function hello(): Response
     {
         return new Response('Hello David');
