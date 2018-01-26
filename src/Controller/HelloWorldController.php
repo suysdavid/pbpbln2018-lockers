3ca355daf69eb5bea9524cdff7c8a9aa60909b7e
David Suys <david@icontroller.eu>
2018-01-26 09:49:22 +0100

    inject twig in controller action instead of extending controller

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -3,18 +3,23 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
-class HelloWorldController extends Controller
+class HelloWorldController
 {
     /**
      * @Route("/hello", name="hello", methods={"GET"})
      *
+     * @param Environment $twig
+     *
      * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
      */
-    public function hello(): Response
+    public function hello(Environment $twig): Response
     {
-        return $this->render('hello.html.twig', ['name' => 'David']);
+        return new Response($twig->render('hello.html.twig', ['name' => 'David']));
     }
 }
